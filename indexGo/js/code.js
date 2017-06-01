942bc808d9f584790cf17d30f4eb3d5d373650ad
Wing <17802598211@163.com>
2017-06-01 08:32:27 +0800

    111

--- indexGo/js/code.js
+++ indexGo/js/code.js
@@ -1,4 +1,39 @@
 /**
  * 
  */
+//jQuery("#b1").click(function(){
+//	alert("I'm running!");
+//	jQuery("#showc").text("I'm running!");
+//	console.log("I'm running!");
+//});
+//<button type="button" id = "b1">实验</button>
+
+var myApp = angular.module('codeApp',[]);
+
+myApp.controller("showCode",function($scope){
+
+//	$http.get('./public/code/linear_list/LinearList.h').then(function(){
+//		console.log(response.data);
+//		$scope.codes = response.data;
+//	});
+	
+//	jQuery("#showc").load("./public/code/linear_list/LinearList.h");
+	jQuery.ajaxSetup({ 		//设置同步，这是为了让全局变量存储
+	    async : false 
+	});  
+	temp = jQuery.get("./public/code/linear_list/LinearList.h",function(data,status){
+////		jQuery("showc").text(data);
+		
+//		$scope.codes = 1111;
+//		console.log(data);
+//		$scope.codes = data;
+//		console.log($scope.codes);
+	});
+	
+	$scope.codes = temp.responseText;
+//	console.log(temp.responseXML);
+	console.log(temp);
+//	console.log($scope.codes);
+	
+});
 
